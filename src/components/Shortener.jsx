c98d05d598c53e0b7c401e9006285a8a804a22f0
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-07-02 10:58:03 -0600

    Added overflow and ellipsis to long url text

--- src/components/Shortener.jsx
+++ src/components/Shortener.jsx
@@ -2,17 +2,17 @@ import { useState, useEffect } from "react";
 import { shortenUrl } from "../assets/shortenUrl";
 
 export function Shortener() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   function handleSave(newUrl) {
-    const isInList = data.some(e => e.shortUrl === newUrl.shortUrl);
+    const isInList = data.some((e) => e.shortUrl === newUrl.shortUrl);
     if (isInList) {
-      setError('The URL already exists.');
+      setError("The URL already exists.");
       return;
-    };
+    }
 
     try {
       const newList = [newUrl, ...data];
@@ -26,7 +26,7 @@ export function Shortener() {
   async function handleShorten(e) {
     e.preventDefault();
     const url = input.trim();
-    if (url === '') return setError('Please add a link');
+    if (url === "") return setError("Please add a link");
     setLoading(true);
     setError(null);
 
@@ -43,7 +43,7 @@ export function Shortener() {
 
   function handleDelete(shortUrl) {
     try {
-      const newList = data.filter(e => e.shortUrl !== shortUrl);
+      const newList = data.filter((e) => e.shortUrl !== shortUrl);
       localStorage.setItem("SHORTENED_LINKS", JSON.stringify(newList));
       setData(newList);
     } catch (error) {
@@ -63,14 +63,17 @@ export function Shortener() {
     }
   }, []);
 
-  const inputError = error ? 'text-red-brand outline-2 outline-red-brand' : 'text-very-dark-violet';
+  const inputError = error
+    ? "text-red-brand outline-2 outline-red-brand"
+    : "text-very-dark-violet";
 
   return (
     <section className="flex flex-col" id="shorten">
       <div className="px-6 bg-[linear-gradient(to_bottom,_white_0%,_white_50%,_#f0f1f6_50%,_#f0f1f6_100%)] mt-15">
         <form
           className="flex flex-col md:flex-row gap-4 rounded-xl p-7 md:p-11 bg-[url(/bg-shorten-mobile.svg)] md:bg-[url(/bg-shorten-desktop.svg)] bg-size-[75%] md:bg-cover md:bg-center md:bg-fit bg-no-repeat bg-top-right bg-violet-brand max-w-5xl w-full mx-auto md:items-center"
-          onSubmit={handleShorten}>
+          onSubmit={handleShorten}
+        >
           <div className="flex flex-col gap-2 w-full md:relative">
             <input
               className={`py-3 px-4 bg-white rounded-lg w-full ${inputError}`}
@@ -80,20 +83,23 @@ export function Shortener() {
                 if (error) setError(null);
                 setInput(event.target.value);
               }}
-              ></input>
-            {error && 
-              <p className="text-red-400 text-sm italic md:absolute md:-bottom-7">{error}</p>
-            }
+            ></input>
+            {error && (
+              <p className="text-red-400 text-sm italic md:absolute md:-bottom-7">
+                {error}
+              </p>
+            )}
           </div>
 
           <button
             className="text-white text-center font-bold rounded-lg bg-cyan-brand h-fit p-3 w-full md:w-55 cursor-pointer hover:bg-cyan-hover transition text-xl"
-            type="submit">
-            {loading ? 'Generating...' : 'Shorten It!' }
+            type="submit"
+          >
+            {loading ? "Generating..." : "Shorten It!"}
           </button>
         </form>
       </div>
-      
+
       <LinksList data={data} handleDelete={handleDelete} />
     </section>
   );
@@ -101,33 +107,45 @@ export function Shortener() {
 
 function UrlDiv({ data, handleDelete }) {
   const [copied, setCopied] = useState(false);
-  const bgColor = copied ? 'bg-violet-brand' : 'bg-cyan-brand hover:bg-cyan-hover';
-  const text = copied ? 'Copied!' : 'Copy';
+  const bgColor = copied
+    ? "bg-violet-brand"
+    : "bg-cyan-brand hover:bg-cyan-hover";
+  const text = copied ? "Copied!" : "Copy";
 
   async function handleCopy() {
     try {
       await navigator.clipboard.writeText(data.shortUrl);
-      setCopied(state => !state);
+      setCopied((state) => !state);
       setTimeout(() => setCopied(false), 3000);
     } catch (err) {
-      console.error('Failed to copy text: ', err);
-      alert('Error copying to clipboard. Try again.');
+      console.error("Failed to copy text: ", err);
+      alert("Error copying to clipboard. Try again.");
     }
   }
 
   return (
     <div className="flex flex-col md:flex-row md:items-center gap-3 md:gap-5 rounded-lg bg-white pb-4 md:px-5 md:py-2 max-w-5xl w-full mx-auto shadow-xs">
-      <p className="text-very-dark-violet px-4 md:px-0 py-3 md:py-0 border-b-1 md:border-b-0 border-b-neutral-200 md:w-full">{data.fullUrl}</p>
-      <a href={`https://${data.shortUrl}`} target="_blank" className="text-cyan-brand px-4 md:px-0 md:min-w-fit hover:underline">{data.shortUrl}</a>
+      <p className="text-very-dark-violet px-4 md:px-0 py-3 md:py-0 border-b-1 md:border-b-0 border-b-neutral-200 md:w-full text-ellipsis overflow-scroll whitespace-nowrap">
+        {data.fullUrl}
+      </p>
+      <a
+        href={`https://${data.shortUrl}`}
+        target="_blank"
+        className="text-cyan-brand px-4 md:px-0 md:min-w-fit hover:underline"
+      >
+        {data.shortUrl}
+      </a>
       <div className="flex w-full md:w-fit gap-2 px-4 md:px-0 items-center">
         <button
           className={`rounded-lg font-bold text-white p-3 md:p-2 w-full md:min-w-20 md:w-20 ${bgColor} cursor-pointer transition text-sm`}
-          onClick={handleCopy}>
+          onClick={handleCopy}
+        >
           {text}
         </button>
         <button
           className="rounded-lg font-bold text-white p-3 md:p-2 w-full md:min-w-20 md:w-20 bg-red-400 hover:bg-red-500 cursor-pointer transition text-sm"
-          onClick={() => handleDelete(data.shortUrl)}>
+          onClick={() => handleDelete(data.shortUrl)}
+        >
           Delete
         </button>
       </div>
@@ -140,12 +158,9 @@ function LinksList({ data, handleDelete }) {
 
   return (
     <section className="flex flex-col gap-3 p-6 bg-gray-section">
-      {data.map(url => 
-        <UrlDiv
-          key={url.shortUrl}
-          data={url}
-          handleDelete={handleDelete} />
-      )}
+      {data.map((url) => (
+        <UrlDiv key={url.shortUrl} data={url} handleDelete={handleDelete} />
+      ))}
     </section>
   );
 }
